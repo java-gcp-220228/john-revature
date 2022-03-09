47d2850617294c4deb0d888f34f2b4e78a25c4a7
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2022-03-09 15:45:30 -0500

    Add logging

--- app/src/main/java/com/revature/main/Server.java
+++ app/src/main/java/com/revature/main/Server.java
@@ -5,10 +5,19 @@ import com.revature.controller.ClientController;
 import com.revature.controller.Controller;
 import com.revature.controller.ExceptionController;
 import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Server {
+
+    public static Logger logger = LoggerFactory.getLogger(Server.class);
+
     public static void main(String[] args) {
         Javalin app = Javalin.create();
+
+        app.before((ctx) -> {
+            logger.info(ctx.method() + " request received for " + ctx.path());
+        });
 
         map(app, new ClientController(), new AccountController(), new ExceptionController());
 
