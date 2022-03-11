1d9dea320d832d2ac870da89b339d05969f416c6
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2022-03-11 15:41:22 -0500

    install frontend

--- app/src/main/java/com/revature/main/Server.java
+++ app/src/main/java/com/revature/main/Server.java
@@ -7,6 +7,7 @@ import com.revature.controller.ExceptionController;
 import com.revature.dao.ClientDao;
 import io.javalin.Javalin;
 import io.javalin.http.NotFoundResponse;
+import io.javalin.http.staticfiles.Location;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +16,9 @@ public class Server {
     public static Logger logger = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create(javalinConfig -> {
+            javalinConfig.addStaticFiles("../ui/dist/ui/",Location.EXTERNAL);
+        });
 
         app.before((ctx) -> {
             logger.info(ctx.method() + " request received for " + ctx.path());
