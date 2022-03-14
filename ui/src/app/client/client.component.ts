abaa914f9ad16aee1162f41fc5d1f2d003c72109
Khoi Nguyen Le <kh.nguyen.le@gmail.com>
2022-03-14 15:09:48 -0400

    set data in client comp on init

--- ui/src/app/client/client.component.ts
+++ ui/src/app/client/client.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
@@ -10,16 +10,18 @@ import { ClientDataSource, ClientItem } from './client-datasource';
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.css']
 })
-export class ClientComponent implements AfterViewInit {
+export class ClientComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<ClientItem>;
-  dataSource: ClientDataSource;
+  dataSource!: ClientDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'firstName', 'lastName', 'age'];
 
-  constructor(private api: ApiService) {
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
     this.dataSource = new ClientDataSource(this.api.getClients());
   }
 
